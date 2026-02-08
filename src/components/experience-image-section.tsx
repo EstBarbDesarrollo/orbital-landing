85365ad073bda104b1e8e0145a8f21ee20017308
est_barb_desarrollo <estbarbdesarrollo@gmail.com>
2026-02-08 22:37:41 +0000

    No. Necesito que las áreas cliqueables coincidan exactamente con los dos

--- src/components/experience-image-section.tsx
+++ src/components/experience-image-section.tsx
@@ -20,13 +20,13 @@ export function ExperienceImageSection() {
           {/* Invisible link for "Solicitar atención personalizada" */}
           <Link
             href="#"
-            className="absolute top-[55%] left-[37%] w-[25%] h-[12%] z-10"
+            className="absolute top-[68%] left-[37%] w-[16%] h-[10%] z-10"
             aria-label="Solicitar atención personalizada"
           />
           {/* Invisible link for "Ingresar a Orbital" */}
           <Link
             href="#"
-            className="absolute top-[55%] left-[64%] w-[18%] h-[12%] z-10"
+            className="absolute top-[68%] left-[55%] w-[11%] h-[10%] z-10"
             aria-label="Ingresar a Orbital"
           />
         </>
