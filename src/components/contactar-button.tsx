acf2588e192dbb6bded5c68ecac6bad45d834de6
est_barb_desarrollo <estbarbdesarrollo@gmail.com>
2026-02-08 13:45:13 +0000

    Contactar un asesor no es un hipervínculo. Es solo texto.

--- src/components/contactar-button.tsx
+++ src/components/contactar-button.tsx
@@ -1,16 +1,14 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 export function ContactarButton() {
   return (
-    <Link href="#" className="inline-block">
+    <div className="inline-block">
       <Image
         src="/contactar-asesor-btn.png"
         alt="Contactar un asesor"
         width={190}
         height={48}
-        className="transition-transform hover:scale-105"
       />
-    </Link>
+    </div>
   );
 }
