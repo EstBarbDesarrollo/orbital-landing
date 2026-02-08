60e01a75148fb5e75cb18dd015ab837b1dbd5770
est_barb_desarrollo <estbarbdesarrollo@gmail.com>
2026-02-08 22:39:55 +0000

    Vamos a desplazarlos hacia abajo en 20 px, y hacia la izquierda en 60 px

--- src/components/experience-image-section.tsx
+++ src/components/experience-image-section.tsx
@@ -20,13 +20,13 @@ export function ExperienceImageSection() {
           {/* Invisible link for "Solicitar atención personalizada" */}
           <Link
             href="#"
-            className="absolute top-[68%] left-[37%] w-[16%] h-[10%] z-10 border-2 border-black"
+            className="absolute top-[68%] left-[37%] w-[16%] h-[10%] z-10 border-2 border-black -translate-x-[60px] translate-y-[20px]"
             aria-label="Solicitar atención personalizada"
           />
           {/* Invisible link for "Ingresar a Orbital" */}
           <Link
             href="#"
-            className="absolute top-[68%] left-[55%] w-[11%] h-[10%] z-10 border-2 border-black"
+            className="absolute top-[68%] left-[55%] w-[11%] h-[10%] z-10 border-2 border-black -translate-x-[60px] translate-y-[20px]"
             aria-label="Ingresar a Orbital"
           />
         </>
