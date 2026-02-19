eda5bcbc301636dcf73306548a3523f41a74d7fc
est_barb_desarrollo <estbarbdesarrollo@gmail.com>
2026-02-19 12:54:07 +0000

    Necesito lo siguiente: quiero anclar una imagen png al vértice superior

--- src/components/digitalize-section.tsx
+++ src/components/digitalize-section.tsx
@@ -9,7 +9,7 @@ export function DigitalizeSection() {
   return (
     <section className="relative">
       <Image
-        src="/digitaliza-features.png"
+        src="/digitaliza-features_.png"
         alt="Características de Orbital: Digitalizá, Ordená, Agilizá, Centralizá"
         width={1920}
         height={827}
@@ -27,7 +27,65 @@ export function DigitalizeSection() {
             />
         </div>
       )}
-      <div className="absolute top-[50%] left-[50%] w-[35%] h-[10%]">
+      {/* Botón 1 con imagen anclada */}
+      <div className="absolute top-[19%] left-[57%] w-[35%] h-[12%]">
+        <div className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 z-20">
+          <Image 
+            src="/1_firma_black.png" 
+            alt="Firma" 
+            width={45} 
+            height={45} 
+            className="object-contain"
+          />
+        </div>
+        <Button 
+          asChild 
+          variant="outline" 
+          className="w-full h-full bg-transparent border-black hover:bg-black/5 rounded-none"
+        >
+          <Link href="#">
+            <span className="sr-only">Acción Digitaliza</span>
+          </Link>
+        </Button>
+      </div>
+      {/* Botón 2 */}
+      <div className="absolute top-[34.7%] left-[57%] w-[35%] h-[12%]">
+        <Button 
+          asChild 
+          variant="outline" 
+          className="w-full h-full bg-transparent border-black hover:bg-black/5 rounded-none"
+        >
+          <Link href="#">
+            <span className="sr-only">Acción Digitaliza</span>
+          </Link>
+        </Button>
+      </div>
+      {/* Botón 3 */}
+      <div className="absolute top-[50.4%] left-[57%] w-[35%] h-[12%]">
+        <Button 
+          asChild 
+          variant="outline" 
+          className="w-full h-full bg-transparent border-black hover:bg-black/5 rounded-none"
+        >
+          <Link href="#">
+            <span className="sr-only">Acción Digitaliza</span>
+          </Link>
+        </Button>
+      </div>
+      {/* Botón 4 */}
+      <div className="absolute top-[66.1%] left-[57%] w-[35%] h-[12%]">
+        <Button 
+          asChild 
+          variant="outline" 
+          className="w-full h-full bg-transparent border-black hover:bg-black/5 rounded-none"
+        >
+          <Link href="#">
+            <span className="sr-only">Acción Digitaliza</span>
+          </Link>
+        </Button>
+      </div>
+      {/* Botón 5 */}
+      <div className="absolute top-[81.8%] left-[57%] w-[35%] h-[12%]">
         <Button 
           asChild 
           variant="outline" 
